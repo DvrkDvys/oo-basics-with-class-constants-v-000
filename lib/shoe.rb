63ff5ee3bb701c4c01301f5f010ad78b0018da9e
Jordan Harris <jharris@wework.com>
2018-07-11 18:30:51 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,9 +14,9 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    if BRAND.include?(brand) == false
+    # if BRAND.include?(brand) == false
       BRAND << brand 
-    end
+    # end
   end
 end
 
