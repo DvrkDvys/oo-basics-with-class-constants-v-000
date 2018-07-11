5766481e77385d9e2f38c9743f9bd3903e5d6c69
Jordan Harris <jharris@wework.com>
2018-07-11 18:45:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,8 +5,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRAND.include?(brand) == false
-          BRANDS << brand 
+    if BRANDS.include?(brand) == false
+        BRANDS << brand 
     end
   end
 
